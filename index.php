27538f59f89bd200579c1252ff289597ad4bcb0e
Önder Erol <onder.erol@gmail.com>
2022-05-27 00:46:28 +0300

    Update index.php
    call types

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
+
 // REQUIREMENTS
 require_once './package.php';
 // MAIN PROGRAMM
 
-// INSTANTIATION
-$cookie = new cookie_session('user', 'devcarle@gmail.com', time() + 15 * 3);
-
-// LAUNCH
-$answer = $cookie->start();
-header("Location: ./");
+// START
+$cookie = new CookieSession('user');
 
 // UPDATE
-$answer = $cookie->update(time() + 60 * 60);
+$answer = $cookie->update('devcarle@gmail.com', time() + 60 * 60);
+/* Set-Cookie: user=devcarle%40gmail.com; expires=Thu, 26-May-2022 22:43:26 GMT; Max-Age=3600; path=/ */
 
-// GET VALUE
-$cookie_value = $cookie->get_value();
-#echo "hey";
+// UPDATE
+$answer = $cookie->update('newuser2@gmail.com');
+/* Set-Cookie: user=newuser2%40gmail.com; expires=Fri, 26-May-2023 21:43:26 GMT; Max-Age=31536000; path=/ */
+
+// READ
+$answer = $cookie->read();
+var_dump($answer);
+
 // DESTRUCTION
 $answer = $cookie->stop();
+/* Set-Cookie: user=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT; Max-Age=0; path=/ */
+
+
